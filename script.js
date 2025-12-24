38e04925b4f489d0736560cd8968ab1a37a81fa5
Dikesh Sapkota <dikeshsapkota@Dikeshs-MacBook-Pro.local>
2025-12-24 15:48:22 +0545

    Update BlackJack project: fixed game logic and styling

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let firstCard=14;
-let secondCard= 7;
-let sum = firstCard + secondCard +5;
 
 let hasBlackJack=false;
 let isAlive=true;
@@ -8,9 +5,14 @@ let message="";
  
 
 function startGame(){
+    let firstCard= Number(document.getElementById("first-Card").value);
+let secondCard= Number(document.getElementById("second-Card").value);
+let sum = firstCard + secondCard;
+//popup displaying cards and sum
    document.getElementById("cards-El").textContent="cards are"+firstCard + " and " + secondCard;
     console.log("game started");
 document.getElementById("sum-el").textContent="sum is:"+sum;
+//checking the conditions
 if(sum > 21){
    
     message="you are out of the game";
@@ -25,7 +27,22 @@ else if(sum< 21){
     hasBlackJack=true;
    
 }
-//cash out using boolean
+
 
 document.getElementById("message-el").textContent="message: " + message;
+}
+function newGame(){
+    //reset game states
+   let hasBlackJack=false;
+let isAlive=true;
+let message="";
+//popup clearing
+
+document.getElementById("cards-El").textContent="";
+document.getElementById("sum-el").textContent="";
+document.getElementById("message-el").textContent="";
+//clearing card inputs
+document.getElementById("first-Card").value="";
+document.getElementById("second-Card").value="";
+ 
 }
