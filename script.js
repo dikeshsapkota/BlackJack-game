7d800d90579005ec83e25026cc9a136c2bfb30be
Dikesh Sapkota <dikeshsapkota@Dikeshs-MacBook-Pro.local>
2025-12-25 11:33:40 +0545

    using array for displaying cards

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+//cards in array
+let cards=[];
 let hasBlackJack=false;
 let isAlive=true;
 let message="";
@@ -25,17 +26,21 @@ document.getElementById("message-el").textContent="message: " + message;
 }
 
 function startGame(){
-    let firstCard= Number(document.getElementById("first-Card").value);
-let secondCard= Number(document.getElementById("second-Card").value);
-sum = firstCard + secondCard;
-//popup displaying cards and sum
-   document.getElementById("cards-El").textContent="cards are"+firstCard + " and " + secondCard;
+    
+   
+ firstCard= Number(document.getElementById("first-Card").value);
+ secondCard= Number(document.getElementById("second-Card").value);
+ //update cards array
+cards=[firstCard,secondCard];
+ sum = firstCard + secondCard;
+//popup displaying cards using array and sum
+for(let i=0;i<cards.length;i++){
+   document.getElementById("cards-El").textContent+=cards[i]+" ";}
     console.log("game started");
 document.getElementById("sum-el").textContent="sum is:"+sum;
 checkGame();
-
-
 }
+//resetting the game
 function newGame(){
     //reset game states
    let hasBlackJack=false;
@@ -52,13 +57,16 @@ document.getElementById("first-Card").value="";
 document.getElementById("second-Card").value="";
  
 }
+//drawing new card
 function newCard(){
 if (isAlive && !hasBlackJack) {
     console.log("drawing a new card from the deck");
         let newCard=3;
+        
         sum += newCard;
+        cards.push(newCard);
         document.getElementById("sum-el").textContent="sum is:"+ sum;
-document.getElementById("cards-El").textContent +=" new card is:" + newCard;
+document.getElementById("cards-El").textContent +=" " + newCard;
 checkGame();}
 
 
